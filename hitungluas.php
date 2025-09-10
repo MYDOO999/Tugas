34ed5f03463c23fb50f23fdb11122ace6ad9907a
MYDOO999 <ridhopribadialislami@gmail.com>
2025-09-10 11:40:15 +0700

    filehitungluas

---
+++ hitungluas.php
@@ -0,0 +1,17 @@
+<?php
+echo "<h1>menghitung luas persegi panjang</h1>";
+$panjang = 50;
+$lebar = 45;
+$luas = $panjang * $lebar;
+$keliling = 2 * ($panjang + $lebar);
+echo "panjang : $panjang";
+echo "<br>";
+echo "lebar : $lebar";
+echo "<br>";
+echo "Luas Persegi panjang : $luas";
+
+echo "<br><br>";
+echo "Rumus Keliling Persegi Panjang = 2 * ( panjang + lebar )";
+echo "<br>";
+echo "Keliling Persegi Panjang : $keliling";
+?>
